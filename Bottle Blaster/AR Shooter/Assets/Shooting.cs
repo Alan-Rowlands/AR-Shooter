ae2c21de86e43c09c7830897b271feceef91b633
Alan-Rowlands <K00194902@student.lit.ie>
2019-02-13 18:18:07 +0000

    AR Shooter

---
+++ Bottle Blaster/AR Shooter/Assets/Shooting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    public GameObject Explosion;
+    public GameObject ARCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Shoot()
+    {
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ARCamera.transform.position, ARCamera.transform.forward, out hit))
+        {
+
+            if(hit.transform.name == "Coke Bottle")
+            {
+                Destroy(hit.transform.gameObject);
+                GameObject go = Instantiate(Explosion, hit.point, Quaternion.LookRotation(hit.normal));
+            }
+        }
+
+    }
+
+}
